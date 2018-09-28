cb5066c55043e9aa97c0e55f2c82262fc95e6e2b
Rishov Chatterjee <garpchoo@gmail.com>
2018-09-28 04:32:44 -0700

    adding shaheer folder

---
+++ shaheer/example.py
@@ -0,0 +1,5 @@
+from stats.mode import mode
+from stats.mean import mean
+
+print(mode([1,2,1,4,5,2,6,2,10,15,13,17,16,13,18,22,17,18,19]))
+print(mean([1,2,3,4,5,6]))
---
+++ shaheer/stats/mode.py
@@ -0,0 +1,3 @@
+def mode(x):
+    x.sort()
+    return x[-1] - x[0]
