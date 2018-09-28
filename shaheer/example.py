b4f0d7c0998ab9059a63f80e03473a9f5cca4b69
Rishov Chatterjee <garpchoo@gmail.com>
2018-09-28 04:36:06 -0700

    adding final changes to example.py

--- shaheer/example.py
+++ shaheer/example.py
@@ -1,5 +1,7 @@
 from stats.mode import mode
 from stats.mean import mean
+from stats.stdv import stdv
 
 print(mode([1,2,1,4,5,2,6,2,10,15,13,17,16,13,18,22,17,18,19]))
 print(mean([1,2,3,4,5,6]))
+print(stdv([1,2,3,4,5]))
