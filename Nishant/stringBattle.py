d1c6e44816b0361556016b9b40eae9999b9df24a
Rishov Chatterjee <garpchoo@gmail.com>
2018-12-02 23:52:56 -0800

    Getting the array of letters

--- Nishant/stringBattle.py
+++ Nishant/stringBattle.py
@@ -8,7 +8,16 @@ def stringBattle_sliced(factor):
     newStr = myString[0] + (newLength * myString[1:])
     return newStr
 
-print(stringBattle_sliced(5))
 
 
 def stringBattle_NLetters(factor):
+    if not isinstance(factor, int or float):
+        raise TypeError
+    myString = input("Type in your opponent's text: ")
+    if not isinstance(myString, str):
+        raise TypeError
+    letterArray = list(myString)
+    print(letterArray)
+    return letterArray
+
+stringBattle_NLetters(5);
