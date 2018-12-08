f2d8f3b837f185dcb40e9751bd92a328e3fc5c07
Rishov Chatterjee <garpchoo@gmail.com>
2018-12-08 11:42:30 -0800

    modifying

--- Obim/bits.js
+++ Obim/bits.js
@@ -1,3 +1,13 @@
 myVar = "0101"
 myVarFlipped = ~parseInt(myVar,2)
 console.log(myVarFlipped)
+
+
+function dec2bin(dec){
+    return (dec >>> 0).toString(2);
+}
+
+console.log(dec2bin(256))
+console.log(dec2bin(-256))
+
+
