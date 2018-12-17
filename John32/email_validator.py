abf14136ac6a1444de14dbdb21fa981085b5572f
Rishov Chatterjee <garpchoo@gmail.com>
2018-12-17 13:20:10 -0800

    isinstance

--- John32/email_validator.py
+++ John32/email_validator.py
@@ -1,13 +1,14 @@
 def email_validator():
     input_email = input("Type in a valid email address: ")
-    if not isinstance(input_email, str):
-        raise TypeError
     if '@' in input_email:
         at_index = input_email.find('@')
         email_prefix = input_email[: at_index]
         email_domain = input_email[at_index + 1:]
         if '.' in input_email and email_domain[0] == input_email[at_index + 1]:
             message = "This email is valid."
-        else:
-            message = "Please write a valid email address and try again."
-        return message 
+    else:
+        message = "Please write a valid email address and try again."
+    return message
+print(email_validator())
+print(email_validator())
+print(email_validator())
