24e52b8cd84daccb135bc3726357539811e62de4
Rishov Chatterjee <garpchoo@gmail.com>
2018-12-17 11:17:19 -0800

    editing unpacking.py

---
+++ John32/unpacking.py
@@ -0,0 +1,18 @@
+import re
+
+def stringExtract():
+    text = input("Input some text: ")
+    character_indices = input("What characters do you want to check index: ")
+    indexList = [i.start() and i.end() for i in re.finditer(character_indices, text)]
+    print(indexList)
+    n = int(input("At which index do you want to remove?: "))
+    # Now you can choose which specific character you want to remove
+    extract_text_from_index = text.replace(text[n-1], '')
+    print(f"This is the new text: {extract_text_from_index}")
+    # Want to extract anything else?
+    other_text = input("Input what you want to extract: ")
+    extract_other_text = extract_text_from_index.replace(other_text, '')
+    return extract_other_text
+
+print(stringExtract())
+print(stringExtract())
