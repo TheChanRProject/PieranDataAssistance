15589a2b6c9b6666a7dc5b4bab6a96eae8a9e61a
Rishov Chatterjee <garpchoo@gmail.com>
2018-12-17 11:21:25 -0800

    finishing unpacking

--- John32/unpacking.py
+++ John32/unpacking.py
@@ -7,12 +7,9 @@ def stringExtract():
     print(indexList)
     n = int(input("At which index do you want to remove?: "))
     # Now you can choose which specific character you want to remove
-    extract_text_from_index = text.replace(text[n-1], '')
+    extract_text_from_index = text[:n-1] + text[n:]
     print(f"This is the new text: {extract_text_from_index}")
     # Want to extract anything else?
-    other_text = input("Input what you want to extract: ")
+    other_text = input("Input what else you want to extract: ")
     extract_other_text = extract_text_from_index.replace(other_text, '')
     return extract_other_text
-
-print(stringExtract())
-print(stringExtract())
