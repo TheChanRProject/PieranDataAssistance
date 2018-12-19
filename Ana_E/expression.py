4fb59551c01d8d2ec6ca8e93ac882eb6da395d7f
Rishov Chatterjee <garpchoo@gmail.com>
2018-12-18 18:26:24 -0800

    adding some stuff to expression.py

--- Ana_E/expression.py
+++ Ana_E/expression.py
@@ -1,2 +1,6 @@
 from sympy import *
 from sympy.abc import x
+from sympy.parsing.sympy_parser import parse_expr, standard_transformations, implicit_multiplication_application
+
+def build_base_function():
+    return float(input("Write in a mathematical function using x for variable: "))
