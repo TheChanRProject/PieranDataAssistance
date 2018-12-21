07f36d8e845fbdecb6c3e8196b844bc8d27c42a1
Rishov Chatterjee <garpchoo@gmail.com>
2018-12-21 14:02:54 -0800

    hydrogen boi

--- sneakersupreme2324/f.py
+++ sneakersupreme2324/f.py
@@ -1,2 +1,17 @@
-name = 'Rishov'
-print(f"Hi I am {name}.")
+class Book():
+
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+    def __len__(self):
+        return self.pages
+
+
+b = Book('Python rocks', 'Jose', 200)
+
+print(b)
