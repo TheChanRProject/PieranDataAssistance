ad754e954a44fb92f36327b4bd43ee1f5e9d8623
Rishov Chatterjee <garpchoo@gmail.com>
2019-01-06 17:23:13 +0530

    Adjusting height and wdith

--- Learner/abs_dif.py
+++ Learner/abs_dif.py
@@ -3,3 +3,5 @@ from IPython.core.display import display, Markdown, Image
 display(Markdown("# Absolute Value"))
 
 display(Markdown("$$ \\mid n \\mid = \\text{n's distance from 0} $$"))
+
+display(Image('absolute-value.svg', height=500, width=500))
