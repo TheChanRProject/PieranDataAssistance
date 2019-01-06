96cf0ff2a034384c6ca470810480bd6185ec9c15
Rishov Chatterjee <garpchoo@gmail.com>
2019-01-06 17:27:28 +0530

    updating

--- Learner/abs_dif.py
+++ Learner/abs_dif.py
@@ -1,7 +1,7 @@
-from IPython.core.display import display, Markdown, Image
+from IPython.core.display import display, Markdown, HTML
 
 display(Markdown("# Absolute Value"))
 
 display(Markdown("$$ \\mid n \\mid = \\text{n's distance from 0} $$"))
 
-display(Image("PieranDataAssistance/Learner/absolute-value.svg"))
+display(HTML('<img src="absolute-value.svg" />'))
