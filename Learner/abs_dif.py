2644e2ad384927e15733b07ed2f609d62583c9f0
Rishov Chatterjee <garpchoo@gmail.com>
2019-01-06 17:13:30 +0530

    yuh

--- Learner/abs_dif.py
+++ Learner/abs_dif.py
@@ -2,4 +2,4 @@ from IPython.core.display import display, Markdown, Image
 
 display(Markdown("# Absolute Value"))
 
-display(Markdown("$$ \\mid n \\mid $$"))
+display(Markdown("$$ \\mid n \\mid = \\text{n's distance from 0} $$"))
