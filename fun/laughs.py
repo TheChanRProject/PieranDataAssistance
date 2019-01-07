87b162b15d46bc10f9c2a0bacb76101e4213f191
Rishov Chatterjee <garpchoo@gmail.com>
2019-01-08 01:39:02 +0400

    adding fun

---
+++ fun/laughs.py
@@ -0,0 +1,2 @@
+sectionArray = ["Course Overview", "Python Setup", "Python Object and Data Structure Basics", "Python Comparison Operators", "Python Statements", "Methods and Functions", "Milestone Project - 1, Object Oriented Programming"]
+print(len(sectionArray) + 1)
