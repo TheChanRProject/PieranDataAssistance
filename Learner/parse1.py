09a37123ceeb2e6be3233f310856308d0d3d0a01
Rishov Chatterjee <garpchoo@gmail.com>
2019-01-10 09:20:08 -0800

    no errors

--- Learner/parse1.py
+++ Learner/parse1.py
@@ -1,2 +1,9 @@
 import bs4
-import requests 
+import requests
+
+res = requests.get('https://play.google.com/store/apps/details?id=com.uicashmagnet')
+soup = bs4.BeautifulSoup(res.text)
+rob = soup.find_all('<span class="htlgb">')
+
+list1 = [i for i in rob]
+print(list1)
