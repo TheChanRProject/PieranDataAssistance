9958268f1ad76d1eea604b2614091db588ff5400
Rishov Chatterjee <garpchoo@gmail.com>
2019-01-10 10:09:16 -0800

    finished

--- Learner/parse1.py
+++ Learner/parse1.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 
 res = requests.get('https://play.google.com/store/apps/details?id=com.uicashmagnet')
-soup = BeautifulSoup(res.text)
+soup = BeautifulSoup(res.text, 'html.parser')
 results = soup.findAll('span', attrs={'class': 'htlgb'})
-for i in results:
-    print(i)
+
+print(results[1].text.strip())
