6a1a5b75f794a0400041089268d3a73918f7e946
Rishov Chatterjee <garpchoo@gmail.com>
2019-01-09 23:01:02 -0800

    los angeles

---
+++ Manan/practical_exam_ds/practical_exam_rishov.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import tensorflow as tf
+
+# Problem 1
+
+boston_df = pd.read_csv("Manan/practical_exam_ds/data/housing.csv")
+boston_df.head()
