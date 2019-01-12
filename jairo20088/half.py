a1cb50ad5439b467ae29f1db456a51d938315bae
Rishov Chatterjee <garpchoo@gmail.com>
2019-01-12 13:53:15 -0800

    editing

--- jairo20088/half.py
+++ jairo20088/half.py
@@ -1,9 +1,3 @@
 def half(word):
 	if len(word) % 2 == 0:
-		half_index = len(word) / 2
-		newStr = word[0:int(half_index)]
-	else:
-		newStr = "Length of string is not even."
-	return newStr
-
-print(half('ButterPutter'))
+		return word[:int(len(word) / 2)]
