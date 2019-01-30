5aa869402537bf0f5a050d5b4217b636facfbfaa
Rishov Chatterjee <garpchoo@gmail.com>
2019-01-30 03:09:27 -0800

    adding edits

--- VIP_DZ/message_war.py
+++ VIP_DZ/message_war.py
@@ -4,15 +4,20 @@ message = input("Put in your message here: ")
 message_list = list(message)
 
 # Create an ordered set
-
 message_set = OrderedDict.fromkeys(message_list)
-print(list(message_set.keys()))
-# Message Dictionary
 
+# Message Dictionary
 mDict = {}
 for i in message_set:
     mDict[i] = message_list.count(i)
-    print(mDict)
 
-# Adding extra letter
-newMessage = []
+
+print(mDict.items())
+
+newStrList = []
+for key,val in mDict.items():
+    newStrList.append(key*(val+1))
+
+newMessage = ''.join(newStrList)
+
+print(newMessage)
