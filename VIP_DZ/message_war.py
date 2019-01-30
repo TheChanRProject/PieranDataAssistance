c01c845234c1746a7137672a87e02a1a309ce9d8
Rishov Chatterjee <garpchoo@gmail.com>
2019-01-30 01:02:54 -0800

    Crap code

--- VIP_DZ/message_war.py
+++ VIP_DZ/message_war.py
@@ -1,3 +1,12 @@
 message = input("Put in your message here: ")
 message_list = list(message)
-print(message_list)
+
+# Create a dictionary
+message_set = set(message_list)
+print(message_set)
+
+# Set up dictionary
+
+for letter in message_set:
+    message_dict = {letter : message_list.count(letter)}
+    print(message_dict['y'])
