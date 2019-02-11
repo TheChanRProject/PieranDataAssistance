d80d08bdf978cc5d667d64a3f5c2e4d43f1266b0
Rishov Chatterjee <garpchoo@gmail.com>
2019-02-11 15:21:06 -0800

    adding

--- EXO_Blaze/penny.py
+++ EXO_Blaze/penny.py
@@ -1,4 +1,7 @@
+def roundPartial (value, resolution):
+    return round(round(value / resolution) * resolution, 2)
+
 def nickel(l):
-    return [f"${round(i, 1)}" for i in l]
+    return [f"${roundPartial(i, 0.05)}" for i in l]
 
 print(nickel([10.33, 10.36, 10.67, 10.68, 10.65]))
