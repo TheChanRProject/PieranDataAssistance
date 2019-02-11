4e0c23d69fd89e124165ba5748d6b3589ad1e90a
Rishov Chatterjee <garpchoo@gmail.com>
2019-02-11 15:11:44 -0800

    Adding

--- EXO_Blaze/penny.py
+++ EXO_Blaze/penny.py
@@ -1,4 +1,4 @@
 def nickel(l):
-    return [round(i, 1) for i in l]
+    return [f"${round(i, 1)}" for i in l]
 
-print(nickel([10.33, 10.36, 10.66, 10.68, 10.65]))
+print(nickel([10.33, 10.36, 10.67, 10.68, 10.65]))
