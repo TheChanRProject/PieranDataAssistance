2b7dd5bbfe484f4ef322ebb49ed6696f8b834845
Rishov Chatterjee <garpchoo@gmail.com>
2019-04-17 01:19:05 -0700

    Adding edits

--- Shankspy/shanksReg.py
+++ Shankspy/shanksReg.py
@@ -9,3 +9,4 @@ from sklearn.model_selection import cross_validate
 from sklearn.linear_model import LinearRegression
 url = 'https://raw.githubusercontent.com/shanksghub/FootballStats/master/FootballStats.csv'
 df1 = pd.read_csv(url)
+print(f"The columns are {list(df1.columns)}") 
