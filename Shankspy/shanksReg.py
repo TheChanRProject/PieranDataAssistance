7360df7eaaddd599b2260e6e50bacbb7ed3b1cf9
Rishov Chatterjee <garpchoo@gmail.com>
2019-04-17 01:23:21 -0700

    Adding edits

--- Shankspy/shanksReg.py
+++ Shankspy/shanksReg.py
@@ -9,4 +9,12 @@ from sklearn.model_selection import cross_validate
 from sklearn.linear_model import LinearRegression
 url = 'https://raw.githubusercontent.com/shanksghub/FootballStats/master/FootballStats.csv'
 df1 = pd.read_csv(url)
-print(f"The columns are {list(df1.columns)}") 
+print(f"The columns are {list(df1.columns)}")
+print(df1.head())
+
+
+# Functional Approach for Regression Modeling
+def regFunction(a,b):
+    X = np.array(df1[a]).reshape(-1,1)
+    Y = np.array(df1[b]).reshape(-1,1)
+    
