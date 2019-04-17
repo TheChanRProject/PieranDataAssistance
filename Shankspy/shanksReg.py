40f30ddc70129afa0fa6a210a9753c741135b1ba
Rishov Chatterjee <garpchoo@gmail.com>
2019-04-17 01:03:11 -0700

    Adding edits

---
+++ Shankspy/shanksReg.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import plotly.plotly as py
+import plotly.graph_objs as go
+from sklearn import preprocessing
+from sklearn.model_selection import cross_validate
+
+from sklearn.linear_model import LinearRegression
+url = 'https://raw.githubusercontent.com/shanksghub/FootballStats/master/FootballStats.csv'
+df1 = pd.read_csv(url)
