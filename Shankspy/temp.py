d1c1c547c9f8306ec230db7d95144559609ee4c0
Rishov Chatterjee <garpchoo@gmail.com>
2019-04-16 23:55:56 -0700

    Adding edits

---
+++ Shankspy/temp.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_squared_error
+
+def output(x):
+    return 1.8*x + 32
