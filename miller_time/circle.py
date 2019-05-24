b07c261b4b4260392fd419e2729ba7cafa743e19
Rishov Chatterjee <garpchoo@gmail.com>
2019-05-24 11:26:03 -0700

    Adding edits

--- miller_time/circle.py
+++ miller_time/circle.py
@@ -1,12 +1,18 @@
+import math
 class Circle():
 
     #class object attribute
-    pi = 3.14
+
 
     def __init__(self, radius=1):
 
         self.radius = radius
-
+        self.pi = math.pi
     #method
     def get_circumference(self):
-        return (self.radius * self.pi * 2)
+        return round((self.radius * self.pi * 2), 2)
+
+
+circ = Circle()
+
+print(circ.get_circumference())
