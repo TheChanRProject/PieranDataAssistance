9cef5cb9fbf6761a9ae39ec3c85573f2bbe13f05
Rishov Chatterjee <garpchoo@gmail.com>
2019-05-24 15:35:28 -0700

    Adding edits

---
+++ Nishant/nishu.py
@@ -0,0 +1,7 @@
+class Nishant():
+
+    def __init__(self, message) :
+        self.message = input("Type in a message for Nishant to say: ")
+
+    def greeting (self):
+        return "Nishant says " + self.message 
