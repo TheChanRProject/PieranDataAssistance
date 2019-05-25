0854d5da0fd4950a1d06a5462e5a315886224eb5
Rishov Chatterjee <garpchoo@gmail.com>
2019-05-24 22:05:41 -0700

    Adding edits

--- JAsen/github_repo_class.py
+++ JAsen/github_repo_class.py
@@ -1,22 +1,27 @@
 import os
 import re
-
+import shutil
 class Git():
 
     def __init__(self, path, file):
+        os.path.expanduser("~")
         self.path = path
         self.path_files = os.listdir(path)
         for i in self.path_files:
-            if re.match(".txt", i):
-                self.file = open(f"{path}/{file}", "r")
+            if i.endswith(".txt"):
+                self.full_path = os.path.join(path, file)
+                self.file = open(self.full_path, "r")
             else:
                 file_path = input("Please input a file path: ")
-                self.file = open(f"{file_path}/{file}", "r")
+                self.full_path = os.path.join(file_path, file)
+                self.file = open(, "r")
+        os.chdir(self.path)  
+
 
     def clone(self):
         for i in self.file.readlines():
             x = i.replace("\n", "")
-            os.system(f"cd ~/{self.path} && git clone {x}")
+            os.system(f"git clone {x}")
 
 myGit = Git(path="/Volumes/DSDRIVE/DS/Python/Udemy/Jose_Portilla/PieranDataAssistance/JAsen", file="file.txt")
 
