1f82197bd862b799b80a53f694e737c2ef244f11
Rishov Chatterjee <garpchoo@gmail.com>
2019-05-24 22:45:51 -0700

    Adding edits

--- JAsen/github_repo_class.py
+++ JAsen/github_repo_class.py
@@ -1,28 +1,26 @@
 import os
-import re
-import shutil
+
 class Git():
 
-    def __init__(self, path, file):
-        os.path.expanduser("~")
-        self.path = path
-        self.path_files = os.listdir(path)
-        for i in self.path_files:
-            if i.endswith(".txt"):
-                self.full_path = os.path.join(path, file)
-                self.file = open(self.full_path, "r")
-            else:
-                file_path = input("Please input a file path: ")
-                self.full_path = os.path.join(file_path, file)
-                self.file = open(, "r")
-        os.chdir(self.path)  
+    def __init__(self, file):
+        self.path = os.getcwd()
+        self.path_files = os.listdir(self.path)
+        if file in self.path_files:
+            self.full_path = os.path.join(self.path, file)
+            self.file = open(self.full_path, "r")
+        else:
+            file_path = input("Please input a file path: ")
+            self.full_path = os.path.join(file_path, file)
+            self.file = open(self.full_path, "r")
 
 
     def clone(self):
+        os.path.expanduser("~")
+        os.chdir(self.path)
         for i in self.file.readlines():
             x = i.replace("\n", "")
             os.system(f"git clone {x}")
 
-myGit = Git(path="/Volumes/DSDRIVE/DS/Python/Udemy/Jose_Portilla/PieranDataAssistance/JAsen", file="file.txt")
+myGit = Git(file="file.txt")
 
 myGit.clone()
